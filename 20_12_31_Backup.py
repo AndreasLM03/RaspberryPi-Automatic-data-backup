4e1ec13e0974705128cd0ff44c9123c096a13a0f
Andreas Müller <65154369+AndreasLM03@users.noreply.github.com>
2021-02-16 14:19:52 +0100

    Add files via upload

---
+++ 20_12_31_Backup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import re
+import time
+import datetime
+import shutil
+import glob
+import os
+import subprocess
+
+### TIME
+UnixTime = int(time.time())
+print (UnixTime)
+currentdate = datetime.datetime.fromtimestamp(UnixTime).strftime('%Y-%m-%d') # creates a string with current date 
+print(currentdate)
+
+
+output_filename = "PiBackup_" + currentdate
+dir_name = "/home/pi/Dokumente/Programme" # insert your folder which you want to upload into your dropbox cloud
+shutil.make_archive(output_filename, 'zip', dir_name)
+
+time.sleep(10)
+
+
+## Dropbox Upload
+targetPattern = r"/home/pi/*.zip" #search for the current backupfile
+a = glob.glob(targetPattern)[0] #search for the current backupfile
+os.system("/home/pi/Dropbox-Uploader/dropbox_uploader.sh upload " + a + " /Backup/") #upload the current backup file to the dropboxcloud
+
+time.sleep(30)
+
+## Delete zip file on raspberry 
+os.remove(a)
